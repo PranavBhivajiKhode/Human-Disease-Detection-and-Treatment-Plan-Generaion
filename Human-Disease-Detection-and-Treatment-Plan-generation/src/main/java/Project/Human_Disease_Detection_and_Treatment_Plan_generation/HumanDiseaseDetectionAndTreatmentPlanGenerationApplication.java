73aa73e161dd719471c67670d0440324b23846d6
Pranav-Bhivaji-Khode <pranavedu@gmail.com>
2025-01-20 07:57:57 +0530

    Initial Commit

---
+++ Human-Disease-Detection-and-Treatment-Plan-generation/src/main/java/Project/Human_Disease_Detection_and_Treatment_Plan_generation/HumanDiseaseDetectionAndTreatmentPlanGenerationApplication.java
@@ -0,0 +1,14 @@
+package Project.Human_Disease_Detection_and_Treatment_Plan_generation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanDiseaseDetectionAndTreatmentPlanGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanDiseaseDetectionAndTreatmentPlanGenerationApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ Human-Disease-Detection-and-Treatment-Plan-generation/src/test/java/Project/Human_Disease_Detection_and_Treatment_Plan_generation/HumanDiseaseDetectionAndTreatmentPlanGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.Human_Disease_Detection_and_Treatment_Plan_generation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanDiseaseDetectionAndTreatmentPlanGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
